469b6641e433aa6b8511f6efb20335bfe98f2fbb
aboelleef <ahmedaboeleef@trycarriage.com>
2020-01-18 21:19:30 +0300

    add next and previous buttons

--- lib/details_screen.dart
+++ lib/details_screen.dart
@@ -13,7 +13,7 @@ class Details extends StatefulWidget {
 }
 
 class DetailsState extends State<Details> {
-  final String id;
+  String id;
   String pageTitle;
   DetailsState(this.id, this.pageTitle);
   List<dynamic> lines = [
@@ -97,8 +97,7 @@ class DetailsState extends State<Details> {
   ];
   @override
   void initState() {
-    print('before init state in details page ');
-    print(lines);
+    // print('before init state in details page ');
     getPoem();
     super.initState();
   }
@@ -115,17 +114,142 @@ class DetailsState extends State<Details> {
       return chapter['lines'];
     }).toList()[0];
     String pageTitle2 = convert.jsonDecode(response.body)['poem']['name'];
+    int id2 = convert.jsonDecode(response.body)['poem']['id'];
+    setState(() {
+      lines = items;
+      pageTitle = pageTitle2;
+      id = id2.toString();
+    });
+
+    // } else {
+    //   print('Request failed with status: ${response.statusCode}.');
+    // }
+  }
+
+  Future<void> getNextPoem() async {
+    final String url = 'http://www.elborda.com/poems/${id}/next?format=json';
+    var response = await http.get(url);
+    print(convert.jsonDecode(response.body));
+    // if (response.statusCode == 200) {
+
+    List chapters =
+        convert.jsonDecode(response.body)['poem']['chapters'] as List;
+    List items = chapters.map((chapter) {
+      return chapter['lines'];
+    }).toList()[0];
+    String pageTitle2 = convert.jsonDecode(response.body)['poem']['name'];
+    int id2 = convert.jsonDecode(response.body)['poem']['id'];
+    print('----------');
+    // var poemOfDay = DetailPoem.fromJson(convert.jsonDecode(response.body)['poem_of_day']);
+    
+    setState(() {
+      lines = items;
+      pageTitle = pageTitle2;
+      id = id2.toString();
+    });
+
+    // } else {
+    //   print('Request failed with status: ${response.statusCode}.');
+    // }
+  }
+
+  Future<void> getPreviousPoem() async {
+    final String url = 'http://www.elborda.com/poems/${id}/previous?format=json';
+    var response = await http.get(url);
+    print(convert.jsonDecode(response.body));
+    // if (response.statusCode == 200) {
+
+    List chapters =
+        convert.jsonDecode(response.body)['poem']['chapters'] as List;
+    List items = chapters.map((chapter) {
+      return chapter['lines'];
+    }).toList()[0];
+    String pageTitle2 = convert.jsonDecode(response.body)['poem']['name'];
+    int id2 = convert.jsonDecode(response.body)['poem']['id'];
     print('----------');
     // var poemOfDay = DetailPoem.fromJson(convert.jsonDecode(response.body)['poem_of_day']);
     print(items);
     setState(() {
       lines = items;
       pageTitle = pageTitle2;
+      id = id2.toString();
     });
 
     // } else {
     //   print('Request failed with status: ${response.statusCode}.');
     // }
+  }
+
+  void openNext() {
+    print('open next method called');
+    getNextPoem();
+  }
+
+  void openPrevious() {
+    print("open previuos method called");
+    getPreviousPoem();
+  }
+
+  List<Widget> generateBody(List<dynamic> lines) {
+    List<Widget> body = [];
+    print('the vlaue of lines');
+    print(lines);
+    List<Container> items = lines.map((line) {
+      print(line);
+      return Container(
+        alignment: Alignment.center,
+        width: MediaQuery.of(context).size.width - 40,
+        padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        decoration: BoxDecoration(
+            color: Color.fromRGBO(255, 255, 255, 1),
+            border: Border.all(
+              color: Color.fromRGBO(255, 255, 255, 1),
+              width: 2,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(15))),
+        child: Text(
+          line['body'].join('\n'),
+          style: TextStyle(
+            fontSize: 20,
+            color: Color.fromRGBO(0, 100, 0, 1),
+          ),
+        ),
+      );
+    }).toList();
+    body.addAll(items);
+    print("value of body");
+    print(body);
+    body.add(Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        RaisedButton(
+          child: Text(
+            "السابق",
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+            ),
+          ),
+          onPressed: openPrevious,
+          color: Color(0xff4caf50),
+        ),
+        RaisedButton(
+          child: Text(
+            "التالي",
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+            ),
+          ),
+          onPressed: openNext,
+          color: Color(0xff4caf50),
+        ),
+      ],
+    ));
+
+    return body;
   }
 
   @override
@@ -142,28 +266,11 @@ class DetailsState extends State<Details> {
         ),
         child: SingleChildScrollView(
           child: Container(
-                                padding: EdgeInsets.only(bottom: 70),
-
-                      child: Column(
+            padding: EdgeInsets.only(bottom: 70),
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: lines.map((line) {
-                return Container(
-                    alignment: Alignment.center,
-                    width: MediaQuery.of(context).size.width - 40,
-                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                    decoration: BoxDecoration(
-                        color: Color.fromRGBO(255, 255, 255, 1),
-                        border: Border.all(
-                          color: Color.fromRGBO(255, 255, 255, 1),
-                          width: 2,
-                        ),
-                        borderRadius: BorderRadius.all(Radius.circular(15))),
-                    child: Text(line['body'].join('\n'),
-                        style: TextStyle(
-                            fontSize: 20, color: Color.fromRGBO(0, 100, 0, 1))));
-              }).toList(),
+              children: generateBody(lines),
             ),
           ),
         ),
